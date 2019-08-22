cb12a62da663921dc66204dbbbc4ad3b1ed0d3a3
kaushal <0kaushal0@gmail.com>
2019-08-21 22:33:52 -0400

    updated

---
+++ client/src/reducers/auth.js
@@ -0,0 +1,50 @@
+import {
+  REGISTER_SUCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_FAIL,
+  LOGIN_SUCESS,
+  LOGOUT,
+  DELETE_ACCOUNT
+} from "../actions/types";
+const initialState = {
+  token: localStorage.getItem("token"),
+  isAuthenticated: null,
+  loading: true,
+  user: null
+};
+
+export default function(state = initialState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case REGISTER_SUCESS:
+      // case LOGIN_SUCESS :
+      localStorage.setItem("token", payload.token);
+      return {
+        ...state,
+        ...payload,
+        isAuthenticated: true,
+        loading: false
+      };
+    // case AUTH_ERROR:
+    // case LOGIN_FAIL :
+    // case LOGOUT :
+    // case DELETE_ACCOUNT :
+    case REGISTER_FAIL:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false
+      };
+    // case USER_LOADED:
+    //     return {
+    //         ...state, isAuthenticated: true, loading: false, user: payload
+    //     }
+
+    default:
+      return state;
+  }
+}
